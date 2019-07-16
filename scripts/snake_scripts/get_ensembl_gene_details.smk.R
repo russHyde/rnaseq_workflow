18d4225f6013e0f518ec7d79774ba714a30c5f57
Russ Hyde <russHyde@users.noreply.github.com>
2019-07-16 13:16:13 +0100

    Add entrez ids to gene details (#39)
    * add entrez_id column to gene_details

--- scripts/snake_scripts/get_ensembl_gene_details.smk.R
+++ scripts/snake_scripts/get_ensembl_gene_details.smk.R
@@ -24,13 +24,54 @@ get_database <- function(smk){
   )
 }
 
-get_gene_df_from_gtf <- function(smk, reqd_columns){
-  gene_df <- smk@input[["gtf_gz"]] %>%
+get_gene_df_from_gtf <- function(gtf_path, reqd_columns){
+  gene_df <- gtf_path %>%
     parse_gtf() %>%
     get_genes_from_gtf() %>%
     as.data.frame()
 
   gene_df[, reqd_columns]
+}
+
+#' Use biomaRt to convert our primary IDs (ensembl-gene IDs) to entrez gene
+#' IDs, since clusterProfiler, fgsea and ReactomePA use entrez gene IDs to
+#' access annotations.
+#'
+#' Disregard any ensembl ID that maps to multiple entrez gene IDs
+#' Disregard and entrez ID that maps to multiple ensembl gene IDs
+#'
+get_entrez_id_mapping <- function(feature_ids, mart){
+  # returns data-frame (feature_id, entrez_id), where `feature_id` is an
+  # ensembl gene id
+
+  # TODO: use homologiser functions within `get_ensembl_gene_details`
+  ens_to_ent <- biomaRt::getBM(
+    attributes = c("ensembl_gene_id", "entrezgene"),
+    filters = "ensembl_gene_id",
+    values = feature_ids,
+    mart = mart,
+    uniqueRows = TRUE
+  ) %>%
+    dplyr::mutate(entrezgene = as.character(entrezgene)) %>%
+    dplyr::rename(
+      feature_id = ensembl_gene_id,
+      entrez_id = entrezgene
+    )
+
+  duplicated_ens_ids <- with(
+    ens_to_ent, feature_id[duplicated(feature_id)]
+  )
+  duplicated_ent_ids <- with(
+    ens_to_ent, entrez_id[duplicated(entrez_id)]
+  )
+  keep_rows <- with(
+    ens_to_ent,
+    !(feature_id %in% duplicated_ens_ids |
+      entrez_id %in% duplicated_ent_ids
+    )
+  )
+
+  ens_to_ent[keep_rows, ]
 }
 
 main <- function(smk){
@@ -39,20 +80,26 @@ main <- function(smk){
   gtf_columns <- paste(
     "gene", c("id", "version", "name", "source", "biotype"), sep = "_"
   )
-  gene_df <- get_gene_df_from_gtf(smk, gtf_columns)
+  gene_df <- get_gene_df_from_gtf(smk@input[["gtf_gz"]], gtf_columns)
 
   # extract gc content from a biomaRt database
   mart <- get_database(smk)
   gc_percent <- reeq::get_gc_percent(
-    gene_df$gene_id, mart
+    gene_df[["gene_id"]], mart
+  )
+
+  entrez_id_df <- get_entrez_id_mapping(
+    feature_ids = gc_percent[["feature_id"]],
+    mart = mart
   )
 
   # join the gc content to the gene-build metadata and write it to a file
-  results <- merge(
-    gc_percent,
-    gene_df,
-    by.x = "feature_id",
-    by.y = "gene_id"
+  results <- gc_percent %>%
+  merge(
+    gene_df, by.x = "feature_id", by.y = "gene_id"
+  ) %>%
+  merge(
+    entrez_id_df, by = "feature_id", all.x = TRUE
   )
 
   readr::write_tsv(
